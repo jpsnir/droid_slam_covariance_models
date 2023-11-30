f1498ef657894a43fe106cc0d7b032a0e241b5b4
jagatpreet <nir.j@northeastern.edu>
2023-11-29 19:25:30 -0500

    built a linear graph

--- src/python/factor_graph_insights/fg_builder.py
+++ src/python/factor_graph_insights/fg_builder.py
@@ -24,7 +24,7 @@ from pathlib import Path
 from gtsam.symbol_shorthand import L, X
 from functools import partial
 import factor_graph_insights.custom_factors as droid_autogen
-
+import time
 
 # confidence map values will go here.
 
@@ -63,15 +63,28 @@ def print_factor_graph_stats(fg_data: dict):
     print(f"disparity size: {fg_data['disps'].size()}")
 
 
-def convert_to_sym_pose(pose: np.ndarray) -> sym.Pose3:
+def convert_to_gtsam_pose(pose: np.ndarray) -> gtsam.Pose3:
+    '''
+    converts nd array pose to gtsam.Pose3
+    pose in nd array is tx, ty, tz, qw, qx, qy, qz
+    '''
+
+    translation = pose[:3]
+    rotation = gtsam.Rot3(w=pose[6], x=pose[3], y=pose[4], z=pose[5])
+    return gtsam.Pose3(r=rotation, t=translation)
+
+
+def gtsam2sym_pose(pose: gtsam.Pose3) -> sym.Pose3:
     '''
     converts nd array pose from factor graph data to
     sym.pose3 type
     '''
     # assuming pose in this sequence : tx, ty, tz, qx, qy, qz, qw .
-    p = pose[:3]
-    r = sym.Rot3(pose[3:])
-    return sym.Pose3(R=r, t=p)
+    #
+    q = pose.rotation().toQuaternion()  # gtsam type
+    rotation = sym.Rot3(np.array([q.x(), q.y(), q.z(), q.w()]))
+    translation = np.array([pose.x(), pose.y(), pose.z()])
+    return sym.Pose3(R=rotation, t=translation)
 
 
 def convert_to_sym_camera(cam_intrinsics: np.ndarray) -> sym.LinearCameraCal:
@@ -101,10 +114,10 @@ def droid_slam_error_func(
     depth_key = this.keys()[2]
     print(f'Keys accessed : pose_i - {pose_i_key}, pose_j - {pose_j_key},\
             depth_key - {depth_key}')
-    w_pose_i, w_pose_j = v.atVector(pose_i_key), v.atVector(pose_j_key)
+    w_pose_i, w_pose_j = v.atPose3(pose_i_key), v.atPose3(pose_j_key)
     depth = v.atDouble(depth_key)
-    sym_pose_i = convert_to_sym_pose(w_pose_i)
-    sym_pose_j = convert_to_sym_pose(w_pose_j)
+    sym_pose_i = gtsam2sym_pose(w_pose_i)
+    sym_pose_j = gtsam2sym_pose(w_pose_j)
     sym_camera = convert_to_sym_camera(cam_intrinsics)
     (error, jacobian, hessian, rhs) = \
         droid_autogen.droid_slam_residual_single_factor(
@@ -145,12 +158,14 @@ def factor_graph_image_pair(
     # todo check sizes of each matrix
     ROWS = depth.size()[0]
     COLS = depth.size()[1]
-    symbol_xi = gtsam.symbol('X', i)
-    symbol_xj = gtsam.symbol('X', j)
+    symbol_xi = gtsam.symbol('x', i)
+    symbol_xj = gtsam.symbol('x', j)
     if not init_values.exists(symbol_xi):
-        init_values.insert(symbol_xi, pose_i.numpy())
+        pose_i_gtsam = convert_to_gtsam_pose(pose_i.numpy())
+        init_values.insert(symbol_xi, pose_i_gtsam)
     if not init_values.exists(symbol_xj):
-        init_values.insert(symbol_xj, pose_j.numpy())
+        pose_j_gtsam = convert_to_gtsam_pose(pose_j.numpy())
+        init_values.insert(symbol_xj, pose_j_gtsam)
 
     graph = gtsam.NonlinearFactorGraph()
     # Poses are to be optimized.
@@ -161,13 +176,13 @@ def factor_graph_image_pair(
             # each depth in ith camera has to be assigned a symbol
             # as it will be optimized as a variable.
 
-            symbol_di = gtsam.symbol('i', ROWS * COLS * i + count_symbol)
+            symbol_di = gtsam.symbol('d', ROWS * COLS * i + count_symbol)
             if not init_values.exists(symbol_di):
                 init_values.insert(symbol_di, depth[row, col].numpy())
             # define noise for each pixel from confidence map or weight
             # matrix
             w = weights[:, row, col].numpy().reshape(2, 1)
-            pixel_noise_model = gtsam.noiseModel.Diagonal.Sigmas(w)
+            pixel_noise_model = gtsam.noiseModel.Diagonal.Sigmas(1 / w)
             dst_img_coords = target_pt[:, row, col].numpy().reshape(2, 1)
             src_img_coords = np.array([row, col]).reshape(2, 1)
             # define factor for the pixel at (row, col)
@@ -200,7 +215,7 @@ def build_factor_graph(fg_data: dict) -> gtsam.NonlinearFactorGraph:
     ii = graph_data['ii']
     jj = graph_data['jj']
     pair_unique_id = {}
-    n = ii.size()[0]
+    n = 5  # ii.size()[0]
     unique_id = 0
     full_graph = gtsam.NonlinearFactorGraph()
     print(f'''Graph index - {graph_data['ii'].size()},
@@ -239,6 +254,17 @@ def build_factor_graph(fg_data: dict) -> gtsam.NonlinearFactorGraph:
 
 if __name__ == "__main__":
 
+    fg_dir = Path(
+        "/media/jagatpreet/D/datasets/uw_rig/samples"
+    ).joinpath(
+        "woodshole_east_dock_1/factorgraph_data_2023_11_27_16_10_29"
+    )
+    if fg_dir.exists():
+        files_list = sorted(os.listdir(fg_dir))
+    print(f'Number of files = {len(files_list)}')
+    fg_file = fg_dir.joinpath(files_list[0])
+
+    # Prior noise definition for first two poses.
     #  3D rotational standard deviation of prior factor - gaussian model
     #  (degrees)
     prior_rpy_sigma = 1
@@ -254,27 +280,61 @@ if __name__ == "__main__":
                                                                    prior_xyz_sigma]
                                                                   )
                                                          )
-    fg_dir = Path(
-        "/media/jagatpreet/D/datasets/uw_rig/samples"
-    ).joinpath(
-        "woodshole_east_dock_1/factorgraph_data_2023_11_27_16_10_29"
-    )
-    if fg_dir.exists():
-        files_list = sorted(os.listdir(fg_dir))
-    print(f'Number of files = {len(files_list)}')
-    fg_file = fg_dir.joinpath(files_list[0])
+    symbol_first_pose = gtsam.symbol('x', 0)
+    symbol_second_pose = gtsam.symbol('x', 1)
     print(f'Analyzing file : { fg_file}')
     fg_data = import_fg_from_pickle_file(fg_file)
     print_factor_graph_stats(fg_data)
     graph = build_factor_graph(fg_data)
-    symbol_first_pose = gtsam.symbol('X', 0)
-    # graph.push_back(
-    #        gtsam.PriorFactorPose3(
-    #            symbol_first_pose, init_values.atPose3(
+    graph.push_back(
+        gtsam.PriorFactorPose3(symbol_first_pose,
+                               init_values.atPose3(symbol_first_pose),
+                               prior_noise_model)
+    )
+    graph.push_back(
+        gtsam.PriorFactorPose3(symbol_second_pose,
+                               init_values.atPose3(symbol_first_pose),
+                               prior_noise_model)
+    )
+
     print(f'Number of factors={graph.nrFactors()}')
+    number_of_iters = input(
+        'Enter integer number of iterations for optimization:')
+    print(f'Number of iterations {number_of_iters}')
+    time.sleep(2)
     params = gtsam.LevenbergMarquardtParams()
+    params.setMaxIterations(int(number_of_iters))
     print(f" LM params: {params}")
     optimizer = gtsam.LevenbergMarquardtOptimizer(
         graph, init_values, params)
     result = optimizer.optimize()
     print(f'Final result :\n {result}')
+    flag = input("Linearize graph initial values: 0-> No, 1-> yes")
+    jac_list = []
+    b_list = []
+    cov_list = []
+    info_list = []
+    if flag:
+        print(f'Errors init values = {graph.error(init_values)}')
+        lin_graph1 = graph.linearize(init_values)
+        jac, b = lin_graph1.jacobian()
+        cov = np.linalg.inv(jac.transpose() @ jac)
+        info = jac.transpose() @ jac
+    jac_list.append(jac)
+    b_list.append(b)
+    info_list.append(info)
+    cov_list.append(cov)
+    flag = input("Linearize graph final values: 0-> No, 1-> yes")
+    if flag:
+        print(f'Errors final values = {graph.error(result)}')
+        lin_graph2 = graph.linearize(result)
+        jac, b = lin_graph2.jacobian()
+        cov = np.linalg.inv(jac.transpose() @ jac)
+        info = jac.transpose() @ jac
+    jac_list.append(jac)
+    b_list.append(b)
+    info_list.append(info)
+    cov_list.append(cov)
+    if (int(flag)):
+        marginals_init = gtsam.Marginals(graph, init_values)
+        marginals_new = gtsam.Marginals(graph, result)
