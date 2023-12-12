1da30d540ef9b3da941f2940779ffdb76c69e2cb
jagatpreet <nir.j@northeastern.edu>
2023-12-11 22:22:07 -0500

    added two more constructors for building

--- cpp/tests/test_droid_factor_py.py
+++ cpp/tests/test_droid_factor_py.py
@@ -19,26 +19,63 @@ dba_factor = DBA.construct1(1, 2, 3, np.array([10, 20]), np.array([20, 23]))
 print(f' type of object - constructor 1 {type(dba_factor)}')
 
 
-def test_pybind_functionality():
+def test_pybind_init():
     '''
-        checks whether the python package behaves well after building
-        with gtsam types.
-        '''
-    dba_factor = DBA(1, 2, 3, np.array([10., 20.]), np.array([20., 30.]))
+    checks whether the python package behaves well after building
+    with gtsam types.
+    '''
+    dba_factor = DBA(10, 20, 30, np.array([10., 20.]), np.array([20., 30.]))
     print(f' type of object - py::init : {type(dba_factor)}')
+    assert (dba_factor.key_id(1) == 10)
+    assert (dba_factor.key_id1(1) == 10)
+    assert (dba_factor.key_id(2) == 20)
+    assert (dba_factor.key_id1(2) == 20)
+
+
+@pytest.mark.xfail(raises=TypeError)
+@pytest.mark.skip(reason="type conversion of gtsam is not working yet")
+def test_object_builder0():
+    nm = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.2]))
+    dba_factor = DBA.construct0(
+        10, 2, 3, np.array([10, 20]), np.array([20, 23]), nm)
+    print(f' type of object - constructor 0 {type(dba_factor)}')
+    assert (dba_factor.key_id(1) == 10)
+    assert (dba_factor.key_id1(1) == 10)
 
 
 def test_object_builder1():
-    dba_factor = DBA.construct1(1, 2, 3, np.array([10, 20]), np.array([20, 23]))
+    dba_factor = DBA.construct1(10, 2, 3, np.array([10, 20]), np.array([20, 23]))
     print(f' type of object - constructor 1 {type(dba_factor)}')
+    assert (dba_factor.key_id(1) == 10)
+    assert (dba_factor.key_id1(1) == 10)
 
 
-@pytest.mark.skip(reason='segmentation fault, need to understand the reason')
 def test_object_builder2():
-    dba_factor = DBA.construct2(1, 2, 3, np.array([10, 20]), np.array([20, 23]))
+    dba_factor = DBA.construct2(12, 22, 32, np.array([10, 20]), np.array([20, 23]))
     print(f' type of object - constructor2 - {type(dba_factor)}')
 
+    assert (dba_factor.key_id(1) == 12)
+    assert (dba_factor.key_id1(1) == 12)
 
+
+def test_object_builder3():
+    dba_factor = DBA.construct3(10, 21, 32, np.array([10, 20]), np.array([20, 23]))
+    print(f' type of object - constructor 3 {type(dba_factor)}')
+    assert (dba_factor.key_id(1) == 10)
+
+
+@pytest.mark.xfail(raises=TypeError)
+@pytest.mark.skip(reason="type conversion of gtsam is not working yet")
+def test_object_builder4():
+    k = gtsam.Cal3_S2(50., 50., 0, 50., 50.)
+    nm = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.2]))
+    dba_factor = DBA.construct4(
+        10, 21, 32, np.array([10, 20]), np.array([20, 23]), k, nm)
+    print(f' type of object - constructor 4 {type(dba_factor)}')
+    assert (dba_factor.key_id(1) == 10)
+
+
+@pytest.mark.skip(reason="type conversion of gtsam is not working yet")
 @pytest.mark.xfail(raises=Exception)
 def test_compose_rotations():
     '''
@@ -49,7 +86,7 @@ def test_compose_rotations():
     DBA.compose_rotations(r1, r2)
 
 
-@pytest.mark.skip(reason='functionality for types not working')
+@pytest.mark.skip(reason="type conversion of gtsam is not working yet")
 def test_init_with_droid_dba_main_constructor_type():
     '''
  '''
