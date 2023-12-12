33b0c0c7fe25f139a23c416a91ce867322c95a76
jagatpreet <nir.j@northeastern.edu>
2023-12-12 17:01:46 -0500

    tests working without segmentation fault

--- cpp/droid_cvm/wrap/droid_factors_pybinder.cpp
+++ cpp/droid_cvm/wrap/droid_factors_pybinder.cpp
@@ -1,32 +1,33 @@
 #include <boost/shared_ptr.hpp>
 
-#include <boost/smart_ptr/shared_ptr.hpp>
-#include <gtsam/base/serialization.h>
-#include <gtsam/config.h>
-#include <gtsam/geometry/Cal3_S2.h>
-#include <gtsam/linear/NoiseModel.h>
-#include <gtsam/nonlinear/NonlinearFactor.h>
-#include <gtsam/nonlinear/utilities.h> // for RedirectCout.
-#include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
 #include <pybind11/stl_bind.h>
-#include <gtsam/geometry/Rot3.h>
+#include <pybind11/operators.h>
+#include <pybind11/functional.h>
+#include <gtsam/base/serialization.h>
+#include <pybind11/iostream.h>
+#include <memory>
 
 // Droid factor to bind.
 #include "custom_factors/droid_DBA_factor.h"
+#include <gtsam/nonlinear/utilities.h> // for RedirectCout.
+#include <gtsam/nonlinear/NonlinearFactor.h>
+#include <gtsam/linear/NoiseModel.h>
+#include <gtsam/config.h>
+#include <gtsam/geometry/Cal3_S2.h>
+#include <gtsam/geometry/Rot3.h>
 
-BOOST_CLASS_EXPORT(gtsam::GenericValue<gtsam::Cal3_S2>)
-BOOST_CLASS_EXPORT(gtsam::Cal3_S2)
-PYBIND11_DECLARE_HOLDER_TYPE(TYPE_PLACEHOLDER_DONOTUSE,
-                             boost::shared_ptr<TYPE_PLACEHOLDER_DONOTUSE>);
 namespace py = pybind11;
 using namespace droid_factors;
+using namespace std;
 
-
+PYBIND11_DECLARE_HOLDER_TYPE(TYPE_PLACEHOLDER_DONOTUSE, boost::shared_ptr<TYPE_PLACEHOLDER_DONOTUSE>);
 DroidDBAFactor construct(
         Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2,
-          boost::shared_ptr<gtsam::noiseModel::Base> &model) {
+        SharedNoiseModel &model){
     // copy of the object.
+    //noiseModel::Diagonal::shared_ptr casted_model = model.cast<noiseModel::Diagonal::shared_ptr>();
     return DroidDBAFactor(k1, k2, k3, p1, p2, model);
 }
 
@@ -40,7 +41,7 @@ DroidDBAFactor construct1(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point
 auto construct2(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
     // raii style pointer, with complete ownership.
     // conceptually python should have the same memory
-            return unique_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2));
+            return std::unique_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2));
 }
 
 auto construct3(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
@@ -51,8 +52,8 @@ auto construct3(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
 
 auto construct4(
         Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2,
-          Cal3_S2 K, boost::shared_ptr<gtsam::noiseModel::Base> &model) {
-    return unique_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2, K, model));
+          boost::shared_ptr<Cal3_S2> &K) {
+    return boost::shared_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2, K));
 }
 
 int key(const DroidDBAFactor* f, int id){
@@ -81,19 +82,21 @@ PYBIND11_MODULE(droid_factors_py, m) {
     m.def(
         "hello", []() { std::cout << " Hello DROID wrapper\n"; },
         "A function that returns mean of values in eigen vector");
-  py::class_<DroidDBAFactor>(m, "DBA")
+  py::class_<DroidDBAFactor, unique_ptr<DroidDBAFactor>>(m, "DBA")
       .def(py::init<size_t, size_t, size_t, const gtsam::Point2 ,
                     const gtsam::Point2>(),
            py::arg("key1_pi"), py::arg("key2_pj"), py::arg("key3_di"),
-           py::arg("pixel_i"), py::arg("predicted_pixel_j"))
+           py::arg("pixel_i"), py::arg("predicted_pixel_j"), py::return_value_policy::reference)
       .def("construct0",&construct)
-      .def("construct1",&construct1)
-      .def("construct2",&construct2)
-      .def("construct3",&construct3)
-      .def("construct4",&construct4)
+      .def("construct1",&construct1, py::return_value_policy::move)
+      .def("construct2",&construct2, py::return_value_policy::move)
+      .def("construct3",&construct3, py::return_value_policy::move)
+      .def("construct4",&construct4, py::return_value_policy::move)
       .def("key_id", &key)
       .def("key_id1", &key_o)
       .def("compose_rotations",&compose_rotations);
+
+
 }
 //.def(py::init<const Key, const Key, const Key, const gtsam::Point2 ,
 //                const gtsam::Point2, const gtsam::Cal3_S2 ,
