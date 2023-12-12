babc0a2a3c7b5593f6b9125e2c334b2d531b24f4
jagatpreet <nir.j@northeastern.edu>
2023-12-12 17:01:46 -0500

    tests working without segmentation fault

--- cpp/tests/test_droid_factor_py.py
+++ cpp/tests/test_droid_factor_py.py
@@ -10,13 +10,8 @@ from pathlib import Path
 package_path = Path('/home/jagatpreet/workspaces/NEUFR/vins/covariance_modeling')
 sys.path.append(str(package_path.joinpath(
     'droid_slam_covariance_modeling/cpp/build/droid_cvm')))
+import droid_factors_py
 from droid_factors_py import DBA
-
-
-dba_factor = DBA(1, 2, 3, np.array([10., 20.]), np.array([20., 30.]))
-print(f' type of object - py::init : {type(dba_factor)}')
-dba_factor = DBA.construct1(1, 2, 3, np.array([10, 20]), np.array([20, 23]))
-print(f' type of object - constructor 1 {type(dba_factor)}')
 
 
 def test_pybind_init():
@@ -76,19 +71,20 @@ def test_object_builder4():
 
 
 @pytest.mark.skip(reason="type conversion of gtsam is not working yet")
-@pytest.mark.xfail(raises=Exception)
-def test_compose_rotations():
+@pytest.mark.xfail(raises=TypeError)
+def test_compose_rotations1():
     '''
  testing gtsam custom types other than eigen vectors and scalars.
  '''
     r1 = gtsam.Rot3.RzRyRx(np.pi / 2, np.pi / 3, 0)
     r2 = r1.inverse()
+    print(f'compose operation = {r1.compose(r2)}')
     DBA.compose_rotations(r1, r2)
 
 
 @pytest.mark.skip(reason="type conversion of gtsam is not working yet")
 def test_init_with_droid_dba_main_constructor_type():
     '''
- '''
+    '''
     k = gtsam.Cal3_S2(50., 50., 0, 50., 50.)
     nm = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.1, 0.2]))
