bef9851c8859b1ca2cb00c282099338dceb6d0d8
jagatpreet <nir.j@northeastern.edu>
2023-12-11 22:22:07 -0500

    added two more constructors for building

--- cpp/droid_cvm/wrap/droid_factors_pybinder.cpp
+++ cpp/droid_cvm/wrap/droid_factors_pybinder.cpp
@@ -10,6 +10,7 @@
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl_bind.h>
+#include <gtsam/geometry/Rot3.h>
 
 // Droid factor to bind.
 #include "custom_factors/droid_DBA_factor.h"
@@ -20,17 +21,52 @@ PYBIND11_DECLARE_HOLDER_TYPE(TYPE_PLACEHOLDER_DONOTUSE,
                              boost::shared_ptr<TYPE_PLACEHOLDER_DONOTUSE>);
 namespace py = pybind11;
 using namespace droid_factors;
+
+
 DroidDBAFactor construct(
         Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2,
           boost::shared_ptr<gtsam::noiseModel::Base> &model) {
+    // copy of the object.
     return DroidDBAFactor(k1, k2, k3, p1, p2, model);
 }
+
+
 DroidDBAFactor construct1(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
+    // copy of the object.
+    // different memory after copy operation.
             return DroidDBAFactor(k1, k2, k3, p1, p2);
 }
 
 auto construct2(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
-            return boost::shared_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2));
+    // raii style pointer, with complete ownership.
+    // conceptually python should have the same memory
+            return unique_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2));
+}
+
+auto construct3(Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2) {
+    // raw pointer, python takes ownserhip by default unless set.
+    // same memory
+            return (new DroidDBAFactor(k1, k2, k3, p1, p2));
+}
+
+auto construct4(
+        Key k1, Key k2, Key k3, gtsam::Point2 p1, gtsam::Point2 p2,
+          Cal3_S2 K, boost::shared_ptr<gtsam::noiseModel::Base> &model) {
+    return unique_ptr<DroidDBAFactor>(new DroidDBAFactor(k1, k2, k3, p1, p2, K, model));
+}
+
+int key(const DroidDBAFactor* f, int id){
+    int keyval = -1;
+    if (id > 0 && id < f->keys().size())
+        keyval = f->keys()[id-1];
+    return keyval;
+}
+
+int key_o(const DroidDBAFactor &f, int id){
+    int keyval = -1;
+    if (id > 0 && id < f.keys().size())
+        keyval = f.keys()[id-1];
+    return keyval;
 }
 
 void compose_rotations(Rot3 r1, Rot3 r2){
@@ -45,14 +81,18 @@ PYBIND11_MODULE(droid_factors_py, m) {
     m.def(
         "hello", []() { std::cout << " Hello DROID wrapper\n"; },
         "A function that returns mean of values in eigen vector");
-  py::class_<droid_factors::DroidDBAFactor>(m, "DBA")
+  py::class_<DroidDBAFactor>(m, "DBA")
       .def(py::init<size_t, size_t, size_t, const gtsam::Point2 ,
                     const gtsam::Point2>(),
            py::arg("key1_pi"), py::arg("key2_pj"), py::arg("key3_di"),
            py::arg("pixel_i"), py::arg("predicted_pixel_j"))
-      .def("construct",&construct)
+      .def("construct0",&construct)
       .def("construct1",&construct1)
       .def("construct2",&construct2)
+      .def("construct3",&construct3)
+      .def("construct4",&construct4)
+      .def("key_id", &key)
+      .def("key_id1", &key_o)
       .def("compose_rotations",&compose_rotations);
 }
 //.def(py::init<const Key, const Key, const Key, const gtsam::Point2 ,
