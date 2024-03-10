41b25696e19c7c14519e9b601bc96c5788dd29b7
zeno <>
2024-03-10 14:45:57 -0400

    added argument parser and logging

--- python/factor_graph_insights/app.py
+++ python/factor_graph_insights/app.py
@@ -16,14 +16,22 @@ import gtsam
 from pathlib import Path
 from typing import Union, Dict, List
 import logging
+import argparse
 from factor_graph_insights.ba_problem import BAProblem, FactorGraphData
 from factor_graph_insights.graph_analysis import GraphAnalysis
 from matplotlib import pyplot as plt
-
+import signal
+import sys
+logging.basicConfig(level=logging.INFO)
+def signal_handler(sig, frame):
+    print("Pressed Ctrl + C. Exiting")
+    sys.exit(0)
 
 def plot_pose_covariance(
     singular_values: np.array,
     Determinant: np.array = None,
+    image_ids: np.array = None,
+    num:int = None,
 ):
     """plots the covariance of poses for n dimensional pose
        from the singular values of the marginal covariance matrix.
@@ -32,28 +40,40 @@ def plot_pose_covariance(
         singular_values (np.array): _description_
     """
 
-    fig1, ax_theta = plt.subplots(3, 1, figsize=(12, 12))
-    fig2, ax_position = plt.subplots(3, 1, figsize=(12, 12))
+    
+    
     M, N = singular_values.shape
-    x_ = range(0, M)
+    if image_ids is not None:
+        x_ = image_ids    
+    else:
+        x_ = range(0, M)
     labels = ["roll", "pitch", "yaw"]
+    fig1, ax_theta = plt.subplots(3, 1, figsize=(12, 12))
     for i, ax in enumerate(ax_theta):
         ax.plot(x_, singular_values[:, i], "--*")
         ax.set_xlabel("Pose id ", fontsize=18)
         ax.set_ylabel(f"{labels[i]}", fontsize=18)
         ax.grid(visible=True)
     fig1.suptitle(
-        "Trends of singular Values of covariance matrix - angles", fontsize=18
+        f"Trends of singular Values of covariance matrix - angles - {num}", fontsize=18
     )
+    plt.show(block=False)
+    plt.pause(0.5)
+    plt.close()
+
     labels = ["x(m)", "y(m)", "z(m)"]
+    fig2, ax_position = plt.subplots(3, 1, figsize=(12, 12))
     for i, ax in enumerate(ax_position):
         ax.plot(x_, singular_values[:, i + 3], "--*")
         ax.set_xlabel("Pose id ", fontsize=18)
         ax.set_ylabel(f"{labels[i]}", fontsize=18)
         ax.grid(visible=True)
     fig2.suptitle(
-        "Trends of singular Values of covariance matrix - positions", fontsize=18
+        f"Trends of singular Values of covariance matrix - positions - {num}]", fontsize=18
     )
+    plt.show(block=False)
+    plt.pause(0.5)
+    plt.close()
 
     if Determinant is not None:
         fig3, ax = plt.subplots(1, 1, figsize=(12, 12))
@@ -62,46 +82,69 @@ def plot_pose_covariance(
         ax.set_xlabel("Pose id ", fontsize=18)
         ax.set_ylabel(f"Determinant of Sigma (Log space)", fontsize=18)
         ax.grid(visible=True)
-        fig3.suptitle("Trend of determinants of covariance matrix", fontsize=18)
-    plt.show()
+        fig3.suptitle(f"Trend of determinants of covariance matrix - {num}", fontsize=18)
+    plt.show(block=False)
+    plt.pause(1)
+    plt.close()
 
 
 if __name__ == "__main__":
-    N = 2
-    fg_dir = Path("/media/jagatpreet/D/datasets/uw_rig/samples").joinpath(
-        "woodshole_east_dock_1/factorgraph_data_2023_12_27_14_14_35"
-    )
+    signal.signal(signal.SIGINT, signal_handler)
+    ap = argparse.ArgumentParser("Argument parser for factor graph analysis")
+    ap.add_argument("-d","--dir", type=Path, help="folderpath with factorgraphs")
+    ap.add_argument("-s","--start_id", type=int, help="index of first factor graph file")
+    ap.add_argument("-e","--end_id", type=int, help="index of last factor graph file")
+    ap.add_argument("-p","--plot", action="store_true", help="plot graphs")
+    args = ap.parse_args()
+
+    fg_dir = Path(args.dir)
+    start = args.start_id
+    end = args.end_id
     if fg_dir.exists():
         files_list = sorted(os.listdir(fg_dir))
-    print(f"Number of files = {len(files_list[0:N])}")
-
-    for file_num, filename in enumerate(files_list[0:N]):
+    logging.info(f"Number of files = {len(files_list[start:end])}")
+    files_that_worked = []
+    files_that_failed = []
+    for file_num, filename in enumerate(files_list[start:end]):
         fg_file = fg_dir.joinpath(filename)
         fg_data = FactorGraphData.load_from_pickle_file(fg_file)
         ba_problem = BAProblem(fg_data)
         oldest_nodes = ba_problem.get_oldest_poses_in_graph()
         prior_definition = ba_problem.set_prior_definition(pose_indices=oldest_nodes)
+        logging.debug(f"Prior definition : {prior_definition}")
         ba_problem.add_visual_priors(prior_definition)
         graph = ba_problem.build_visual_factor_graph(N_edges=ba_problem.edges)
         init_vals = ba_problem.i_vals
         S_cat = np.zeros([len(ba_problem.poses), 6])
         D_cat = np.zeros([len(ba_problem.poses), 1])
+        node_ids_i, node_ids_j, image_ids = ba_problem.get_node_ids()
+        logging.info(f"Prior node ids: {oldest_nodes}")
+        logging.info(f"node ids - i: size: {len(node_ids_i)} - {node_ids_i}")
+        logging.info(f"node timestamps: size : {len(image_ids)} - {image_ids}")
+
         try:
             analyzer = GraphAnalysis(graph)
             marginals = analyzer.marginals(init_vals)
-            print(f"Files worked : {file_num}")
+            logging.info(f"Files worked : {file_num}")
 
-            for p_id in range(0, len(ba_problem.poses)):
+            symbols = lambda indices: [gtsam.symbol("x", idx) for idx in indices]
+            
+            for p_id in node_ids_i:
                 cov = marginals.marginalCovariance(gtsam.symbol("x", p_id))
                 U, S, V = np.linalg.svd(cov)
                 S_cat[p_id, :] = S
                 d = np.linalg.det(cov)
                 D_cat[p_id, :] = d
-                print(f" marginal covariance = {cov}")
-                print(f" singular values at {p_id} = {S}")
-                print(f" determinant at {p_id} = {d}")
+                logging.debug(f" marginal covariance = {cov}")
+                logging.debug(f" singular values at {p_id} = {S}")
+                logging.debug(f" determinant at {p_id} = {d}")
+                logging.debug(f"Concatenated singular values = {S_cat}")
+            if args.plot:
+                plot_pose_covariance(S_cat, D_cat, image_ids, file_num )        
+            files_that_worked.append(int(filename.split("_")[1]))
         except Exception as e:
-            print(f"File number {file_num}, Filename - {filename}, error code - {e}")
-        print(f"Concatenated singular values = {S_cat}")
-        plot_pose_covariance(S_cat, D_cat)
+            logging.info(f"File number {file_num}, Filename - {filename}, error code - {e}")
+            files_that_failed.append(int(filename.split("_")[1]))
+    logging.info(f"files that worked : {files_that_worked}")
+    logging.info(f"files that failed : {files_that_failed}")
     # print(f" Rank of matrix {np.linalg.matrix_rank(info)}")
