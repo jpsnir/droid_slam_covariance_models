fd85632236aa33e554ece2acad8490eec6ecb2c4
zeno <>
2024-03-14 13:59:37 -0400

    python cmd tools updated

--- python/cmd_tools/run_factor_graph_analysis.sh
+++ python/cmd_tools/run_factor_graph_analysis.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ $# -ne 4 ]; then
+    echo " Usage: ./run_factor_graph_analysis.sh <start_file> <end_file> <far_depth_mono> <far_depth_stereo>"
+    echo " Default values for far_depth for both mono and stereo is 2"
+    exit 0
+fi
+
+start=$1
+end=$2
+far_depth_mono=$3
+far_depth_stereo=$4
 
 data_folder="/data/jagat/processed/"
 eval_set=(
@@ -18,11 +28,19 @@ eval_set=(
 
 # mono
 for dir_name in ${eval_set[@]}; do
-    python ../factor_graph_insights/app.py -d $data_folder/$dir_name/factor_graphs/mono/lba/ -s 0 -e 35\
-	 --loglevel info --near_depth_threshold 0.25 --far_depth_threshold 2 --plot
+    echo "Covariance calculation for the ${dir_name} - mono data"
+    python ../factor_graph_insights/app.py -d $data_folder/$dir_name/factor_graphs/mono/lba/ -s $start -e $end\
+	 --loglevel info --near_depth_threshold 0.25 --far_depth_threshold $far_depth_mono --plot &
 done
 
-# # stereo
-# for dir_name in ${eval_set[@]}; do
-#     python ../factor_graph_insights/app.py -d $data_folder/$dir_name/factor_graphs/stereo/lba/ -s 0 -e 30 --loglevel info --near_depth_threshold 0.25 --far_depth_threshold 2
-# done
+# stereo
+for dir_name in ${eval_set[@]}; do
+    echo "Covariance calculation for the ${dir_name} - stereo data"
+    python ../factor_graph_insights/app.py -d $data_folder/$dir_name/factor_graphs/stereo/lba/ -s $start -e $end\
+	 --loglevel info --near_depth_threshold 0.25 --far_depth_threshold $far_depth_stereo --plot &
+done
+
+wait
+echo " All processes finish "
+
+exit 0
