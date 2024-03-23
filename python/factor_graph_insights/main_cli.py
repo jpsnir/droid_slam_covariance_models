05b1381b16ebd2bc82b7e077b20a9efdf76cccf3
zeno <>
2024-03-23 12:13:41 -0400

    refactored visualize factor graph script

--- python/factor_graph_insights/main_cli.py
+++ python/factor_graph_insights/main_cli.py
@@ -9,7 +9,7 @@ import time
 from pathlib import Path
 import sys
 import numpy as np
-from factor_graph_insights.visualization_utils import Visualizer
+from factor_graph_insights.visualization_utils import BaseVisualizer 
 from factor_graph_insights.process_data import FactorGraphFileProcessor, FgFolderProcessor
 
 def signal_handler(sig, frame):
@@ -34,92 +34,13 @@ def visualize_factor_graphs():
     # ap.add_argument("--stride", type=int, default=2, help="images to skip when computing ids")
     
     args = ap.parse_args()
-    fg_file = Path(args.filepath)
-    metadata = {}
-    metadata['cam_type'] = fg_file.parents[2].stem
-    metadata['dataset_name'] = fg_file.parents[3].stem
-    metadata['parent_folder'] = fg_file.parents[4]
-    metadata['filename'] = fg_file.stem
-    metadata['file_id'] =metadata['filename'].split("_")[1]
-    rel_path = fg_file.relative_to(metadata['parent_folder'])
-    # plot save location 
-    folder_name = str(rel_path).replace("/","_").split(".")[0]
-    metadata['plot_save_location'] = Path(args.save_dir).joinpath(folder_name)
-    metadata['plot'] = args.plot
-    raw_image_folder=Path(f"/data/jagat/euroc/{metadata['dataset_name']}/mav0/cam0/data/")
-    gt_data = Path(f"/data/jagat/processed/evo/{metadata['dataset_name']}/{metadata['cam_type']}/lba/")
+    fg_file_processor = FactorGraphFileProcessor(args)
+    fg_file_processor.initialize_ba_problem()
+    success = fg_file_processor.process()
+    adj_matrix = fg_file_processor.covisibility_graph_to_adj_matrix()
+    BaseVisualizer.plot_trajectory(
+        fg_file_processor.ba_problem.poses, metadata=fg_file_processor.metadata)
     
-    if not metadata['plot_save_location'].exists(): 
-        metadata['plot_save_location'].mkdir(parents=True)
-        
-    if args.log_to_file:
-        logging.basicConfig(filename=str(metadata['plot_save_location'].joinpath("app_2.log")), 
-                            level=args.loglevel.upper())
-    else:
-        logging.basicConfig(level=args.loglevel.upper())
-    
-    logging.info(f"Near depth : {args.near_depth_threshold}") 
-    logging.info(f"Far depth : {args.far_depth_threshold}") 
-    logging.info(f"logging level = {logging.root.level}")
-    logging.info(f"plots will be saved at : {metadata['plot_save_location'].absolute()}")
-     
-    # analyzing all factor graph files within the start and  end range.
-    fg_data = FactorGraphData.load_from_pickle_file(fg_file)
-    logging.info("Building bundle Adjustment problem")
-    ba_problem = BAProblem(fg_data)
-    
-    ba_problem.near_depth_threshold = args.near_depth_threshold
-    ba_problem.far_depth_threshold = args.far_depth_threshold
-
-    oldest_nodes = ba_problem.get_oldest_poses_in_graph()
-    logging.info("set priors")
-    prior_definition = ba_problem.set_prior_definition(pose_indices=oldest_nodes)
-    logging.debug(f"Prior definition : {prior_definition}")
-    ba_problem.add_visual_priors(prior_definition)
-    if args.number_of_edges > 0:
-        graph = ba_problem.build_visual_factor_graph(N_edges=args.number_of_edges)
-    else:
-        graph = ba_problem.build_visual_factor_graph(N_edges=ba_problem.edges)
-    init_vals = ba_problem.i_vals
-    
-    metadata['node_ids'] = ba_problem.get_node_ids(args.number_of_edges)
-    metadata['image_ids'] = ba_problem.get_image_ids()
-    logging.info(f"Prior node ids: {oldest_nodes}")
-    logging.info(f"node ids - i: size: {len(metadata['node_ids'])} - {metadata['node_ids']}")
-    logging.info(f"node timestamps: size : {len(metadata['image_ids'])} - {metadata['image_ids']}")
-    
-    ################
-    
-    adjacency_matrix, node_ids = covisibility_graph_to_adj_matrix(ba_problem.src_nodes, 
-                                                        ba_problem.dst_nodes)
-    adjacency_matrix_data = {'M': adjacency_matrix,
-                             'node_ids': node_ids}
-    Visualizer.plot_trajectory(ba_problem.poses, metadata=metadata)
-    S_cat = np.zeros([len(metadata['node_ids']), 6])
-    D_cat = np.zeros([len(metadata['node_ids']), 1])                                               
-    try:
-        analyzer = GraphAnalysis(graph)
-        marginals = analyzer.marginals(init_vals)
-        symbols = lambda indices: [gtsam.symbol("x", idx) for idx in indices]
-        cov_list = []
-        for i, p_id in enumerate(metadata['node_ids']):
-            cov = marginals.marginalCovariance(gtsam.symbol("x", p_id))
-            cov_list.append(cov)
-            U, S, V = np.linalg.svd(cov)
-            S_cat[i, :] = S
-            d = np.linalg.det(cov)
-            D_cat[i, :] = d
-            logging.debug(f" marginal covariance = {cov}")
-            logging.debug(f" singular values at {p_id} = {S}")
-            logging.debug(f" determinant at {p_id} = {d}")
-            logging.debug(f"Concatenated singular values = {S_cat}")
-            logging.debug(f"Concatenated covariance list : {cov_list}")
-        logging.info("Marginal covariances computation finished")
-        plot_pose_covariance(S_cat, D_cat, adjacency_matrix_data, metadata)
-            
-    except Exception as e:
-        logging.error(f"File number {metadata['file_id']}, Filename - {metadata['filename']}, error code - {e}")
-        time.sleep(2)
 
 def animate():
     """"""
--- python/factor_graph_insights/process_data.py
+++ python/factor_graph_insights/process_data.py
@@ -14,7 +14,8 @@ import logging
 from factor_graph_insights.ba_problem import BAProblem, FactorGraphData
 from factor_graph_insights.graph_analysis import GraphAnalysis
 import sys
-import time 
+import time
+from typing import Tuple, Dict, List
 
 
 class FactorGraphFileProcessor:
@@ -25,16 +26,49 @@ class FactorGraphFileProcessor:
         """
         initializes the factor graph processer with input arguments
         """
-        
+        self.fg_file = Path(args.filepath)
         self.near_depth_threshold = args.near_depth_threshold
         self.far_depth_threshold = args.far_depth_threshold
         self.number_of_edges = args.number_of_edges
-        if self.fg_dir.exists():
-            self.files_list = sorted(os.listdir(self.fg_dir))
-        logging.info(f"Number of files to processed in {self.fg_dir} = {len(self.files_list[start:end])}")
-        self.metadata = {}
+        self.raw_image_folder = args.raw_image_folder
         self.D_cat = None
         self.S_cat = None
+        self.metadata = {}
+        self.metadata['cam_type'] = self.fg_file.parents[2].stem
+        self.metadata['dataset_name'] = self.fg_file.parents[3].stem
+        self.metadata['parent_folder'] = self.fg_file.parents[4]
+        self.metadata['filename'] = self.fg_file.stem
+        self.metadata['file_id'] =self.metadata['filename'].split("_")[1]
+        rel_path = self.fg_file.relative_to(self.metadata['parent_folder'])
+        folder_name = str(rel_path).replace("/","_").split(".")[0]
+        self.metadata['plot_save_location'] = Path(args.save_dir).joinpath(folder_name)
+        self.metadata['plot'] = args.plot
+        # raw_image_folder=Path(f"{self.raw_image_folder}/{self.metadata['dataset_name']}/mav0/cam0/data/")
+        # gt_data = Path(f"/data/jagat/processed/evo/{metadata['dataset_name']}/{metadata['cam_type']}/lba/")
+        if not self.metadata['plot_save_location'].exists(): 
+            self.metadata['plot_save_location'].mkdir(parents=True)
+        if args.log_to_file:
+            logging.basicConfig(filename=str(self.metadata['plot_save_location'].joinpath("app_2.log")), 
+                            level=args.loglevel.upper())
+        else:
+            logging.basicConfig(level=args.loglevel.upper())
+        
+        logging.info(f"Near depth : {args.near_depth_threshold}") 
+        logging.info(f"Far depth : {args.far_depth_threshold}") 
+        logging.info(f"logging level = {logging.root.level}")
+        logging.info(f"plots will be saved at : {self.metadata['plot_save_location'].absolute()}")
+    
+    @property
+    def metadata(self):
+        return self.metadata
+         
+    @property
+    def graph(self):
+        if self.graph is not None:
+            return self.graph
+        else:
+            raise Exception("Graph is not constructed")
+     
     
     def get_covariance_analytics(self) -> Dict:
         return {
@@ -42,12 +76,13 @@ class FactorGraphFileProcessor:
             "cov_singular_values": self.S_cat,
         }
     
-    def covisibility_graph_to_adj_matrix(src_nodes, dst_nodes):
+    def covisibility_graph_to_adj_matrix(self) -> np.array:
         """
         builds an adjaceny matrix from the list of edges.
         returns an upper triangular matrix
         """
-        
+        src_nodes = self.ba_problem.src_nodes
+        dst_nodes = self.ba_problem.dst_nodes
         node_ids = list(set(np.sort(src_nodes.numpy())))
         N = len(node_ids)
         M = np.zeros((N, N))
@@ -58,44 +93,58 @@ class FactorGraphFileProcessor:
                 if(dst.item()==n):
                     col = i
             M[row, col] = 1
-        return np.triu(M), node_ids
+        return np.triu(M)
     
-    def process(self, fg_file: Path) -> bool:
+    def initialize_ba_problem(self) -> Tuple:
         """
-        formulate the factorgraph file as a ba problem 
+        builds a factor graph from the factor graph data.
+        """
+        
+        fg_data = FactorGraphData.load_from_pickle_file(self.fg_file)
+        self.ba_problem = BAProblem(fg_data)
+        
+        # set some parameters for solving.
+        self.ba_problem.near_depth_threshold = self.near_depth_threshold
+        self.ba_problem.far_depth_threshold = self.far_depth_threshold
+        
+        # set priors of the bundle adjustment problem
+        oldest_nodes = self.ba_problem.get_oldest_poses_in_graph()
+        prior_definition = self.ba_problem.set_prior_definition(pose_indices=oldest_nodes)
+        logging.debug(f"Prior definition : {prior_definition}")
+        
+        # build a factor graph.
+        self.ba_problem.add_visual_priors(prior_definition)
+        if self.number_of_edges > 0:
+            self.graph = self.ba_problem.build_visual_factor_graph(N_edges=self.number_of_edges)
+        else:
+            self.graph = self.ba_problem.build_visual_factor_graph(N_edges=self.ba_problem.edges)
+                    
+        # store some more metadata for saving and plotting
+        self.metadata['node_ids'] = self.ba_problem.get_node_ids(self.number_of_edges)
+        self.metadata['image_ids'] = self.ba_problem.get_image_ids()
+        logging.info(f"Prior node ids: {oldest_nodes}")
+        logging.info(f"node ids - i: size: {len(self.metadata['node_ids'])} - {self.metadata['node_ids']}")
+        logging.info(f"node timestamps: size : {len(self.metadata['image_ids'])} - {self.metadata['image_ids']}")
+        
+        return self.ba_problem, self.metadata
+    
+    def process(self) -> bool:
+        """
+        computes covariance of the ba problem at init_vals 
         and then compute its covariance
         return if true is there is no error
         """
         flag = False
-        filename = fg_file.stem
-        fg_data = FactorGraphData.load_from_pickle_file(fg_file)
-        ba_problem = BAProblem(fg_data)
-        ba_problem.near_depth_threshold = self.near_depth_threshold
-        ba_problem.far_depth_threshold = self.far_depth_threshold
-        
-        oldest_nodes = ba_problem.get_oldest_poses_in_graph()
-        prior_definition = ba_problem.set_prior_definition(pose_indices=oldest_nodes)
-        logging.debug(f"Prior definition : {prior_definition}")
-        ba_problem.add_visual_priors(prior_definition)
-        if self.number_of_edges > 0:
-            graph = ba_problem.build_visual_factor_graph(N_edges=self.number_of_edges)
-        else:
-            graph = ba_problem.build_visual_factor_graph(N_edges=ba_problem.edges)
-        init_vals = ba_problem.i_vals
-        self.S_cat = np.zeros([len(ba_problem.poses), 6])
-        self.D_cat = np.zeros([len(ba_problem.poses), 1])
-        node_ids_i = ba_problem.get_node_ids(self.number_of_edges)
-        image_ids = ba_problem.get_image_ids()
-        logging.info(f"Prior node ids: {oldest_nodes}")
-        logging.info(f"node ids - i: size: {len(node_ids_i)} - {node_ids_i}")
-        logging.info(f"node timestamps: size : {len(image_ids)} - {image_ids}")
-
+            
+        # Initialize the matrix for singular values and determinants.
+        self.S_cat = np.zeros([len(self.metadata['node_ids']), 6])
+        self.D_cat = np.zeros([len(self.metadata['node_ids']), 1])
         try:
-            analyzer = GraphAnalysis(graph)
-            marginals = analyzer.marginals(init_vals)
+            analyzer = GraphAnalysis(self.graph)
+            marginals = analyzer.marginals(self.ba_problem.i_vals)
             symbols = lambda indices: [gtsam.symbol("x", idx) for idx in indices]
             cov_list = []
-            for p_id in node_ids_i:
+            for p_id in self.metadata['node_ids']:
                 cov = marginals.marginalCovariance(gtsam.symbol("x", p_id))
                 cov_list.append(cov)
                 U, S, V = np.linalg.svd(cov)
@@ -109,7 +158,7 @@ class FactorGraphFileProcessor:
                 logging.debug(f"Concatenated covariance list : {cov_list}")
             flag = True
         except Exception as e:
-            logging.error(f"Filename - {filename}, error code - {e}")
+            logging.error(f"Filename - {self.metadata['filename']}, error code - {e}")
             time.sleep(2)
         return flag
 
@@ -118,6 +167,7 @@ class FgFolderProcessor(FactorGraphFileProcessor):
     def __init__(self, args):
         super().__init__(args=args)
         self.fg_dir = Path(args.dir)
+        
         self.plot_folder_name = self._set_paths(self.fg_dir, parent_level=3)    
         self.plot_save_location = Path(args.save_dir).joinpath(self.plot_folder_name)
         if not self.plot_save_location.exists(): 
@@ -133,6 +183,9 @@ class FgFolderProcessor(FactorGraphFileProcessor):
     
         self.start = args.start_id
         self.end = args.end_id
+        if self.fg_dir.exists():
+            self.files_list = sorted(os.listdir(self.fg_dir))
+        logging.info(f"Number of files to process in {self.fg_dir} = {len(self.files_list[self.start:self.end])}")
         self.files_that_worked = []
         self.files_that_failed = []
         
